332f1e181efd6f6603cfd180efabe0c440816e9d
duvalbarrett <duvalbarrett@gmail.com>
2023-01-06 18:33:34 -0500

    header/navbar added

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
+import './header.css'
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className="gpt3__header section__padding" id='home'>
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let's Build something amazing with GPT-3 OpenAI
+        </h1>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        <div className='gpt3__header-content__input'> <input type='email' placeholder='Your email address' />
+        <button type='button'>Get started</button></div>
+        <div className='gpt3__header-content__people'><img src={people} alt={people}/>
+        <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+
+        <div className='gpt3__header-image'>
+          <img src={ai} alt={ai} />
+        </div>
+      </div>
     </div>
   )
 }
